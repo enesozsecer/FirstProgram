0277bf0b2a35c5df313de297668720b14b4bb47b
enesozsecer <ensozscr@gmail.com>
2023-10-04 17:22:38 +0300

    Hata

--- Model/Dtos/UserLoginDto/LoginDto.cs
+++ Model/Dtos/UserLoginDto/LoginDto.cs
@@ -9,6 +9,6 @@ namespace Model.Dtos.UserLoginDto
     public class LoginDto
     {
         public string UserName { get; set; }
-        public string UserPassword { get; set; }
+        public string UserPassword { ; set; }
     }
 }
